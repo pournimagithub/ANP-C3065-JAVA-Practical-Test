87210b0873d665c7091a48c2fd277036b76cb58c
Dell <Dell@DESKTOP-8TTO4IP>
2022-12-02 19:14:44 +0530

    version 1 updated

---
+++ 01_Dec_2022_PracticalTest/src/com/mygame/dao/GameDAO.java
@@ -0,0 +1,12 @@
+package com.mygame.dao;
+
+public class GameDAO {
+	
+	static String CityName[]= {"Chennai","Mumbai","Dehli","Kolkata","Hydrabad"};
+	
+	public String[]getCityName(){
+		return CityName;
+	}
+
+
+}
