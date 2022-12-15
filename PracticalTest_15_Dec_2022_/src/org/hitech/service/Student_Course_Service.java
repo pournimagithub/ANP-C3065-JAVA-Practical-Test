8dfe86d09c810dad9821dd83fbdbd8c394ccb7fe
Dell <Dell@DESKTOP-8TTO4IP>
2022-12-15 19:40:09 +0530

    Version 1 updated

--- PracticalTest_15_Dec_2022_/src/org/hitech/service/Student_Course_Service.java
+++ PracticalTest_15_Dec_2022_/src/org/hitech/service/Student_Course_Service.java
@@ -1,5 +1,24 @@
 package org.hitech.service;
 
-public class Student_Course_Service {
+import java.util.HashMap;
 
+import org.hitech.dao.Student_Course_DAO;
+import org.hitech.model.Course;
+import org.hitech.model.Student;
+
+	public class Student_Course_Service {
+		
+	Student_Course_DAO dao=new Student_Course_DAO();
+	
+	public void addStudentCourseDetails(Student s,Course c){
+		dao.addStudentCourseDetails(s, c);
+	}
+	
+	public HashMap<Student,Course> getStudentCourseDetails() {
+		
+		return dao.getStudentCourseDetails();
+	}
+	public void deleteStudentCourseDetails(Student s) {
+		dao.deleteStudentCourseDetails(s);
+	}
 }
